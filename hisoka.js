35ac3c84c3303c3eb7acd3e82b8b8a3df38f5025
Pinzyxz <102290700+Pinzyxz@users.noreply.github.com>
2022-09-15 18:07:46 +0700

    update hisoka.js

--- hisoka.js
+++ hisoka.js
@@ -121,6 +121,29 @@ module.exports = hisoka = async (hisoka, m, chatUpdate, store) => {
 	 { remoteJid: "status@broadcast" } : {}) },
 	 message: { "liveLocationMessage": { "caption":'MarkJembut',"h": `aloo`, 'jpegThumbnail': fs.readFileSync('./lib/hisoka.jpg')}}
 	}
+ ftoko = {
+key: {
+			fromMe: false,
+			participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: "0@s.whatsapp.net" } : {})
+		},
+		message: {
+			"productMessage": {
+				"product": {
+					"productImage":{
+						"mimetype": "image/jpeg",
+						"jpegThumbnail": fs.readFileSync('./lib/hisoka.jpg') //Gambarnye
+					},
+					"title": "Piiw",
+					"description": "Plinnzbot", 
+					"currencyCode": "USD",
+					"priceAmount1000": "2000",
+					"retailerId": "Ghost",
+					"productImageCount": 1
+				},
+				    "businessOwnerJid": `0@s.whatsapp.net`
+		}
+	}
+}
 	    
         // Public & Self
         if (!hisoka.public) {
@@ -211,7 +234,7 @@ Jawablah Pertanyaan Berikut :\n${room.soal}\n\n\nTerdapat ${room.jawaban.length}
 ${isWin ? `Semua Jawaban Terjawab` : isSurender ? 'Menyerah!' : ''}
 ${Array.from(room.jawaban, (jawaban, index) => {
         return isSurender || room.terjawab[index] ? `(${index + 1}) ${jawaban} ${room.terjawab[index] ? '@' + room.terjawab[index].split('@')[0] : ''}`.trim() : false
-    }).filter(v => v).join('\n')}
+    }).filter(v => v).('\n')}
     ${isSurender ? '' : `Perfect Player`}`.trim()
             hisoka.sendText(m.chat, caption, m, { contextInfo: { mentionedJid: parseMention(caption) }}).then(mes => { return _family100['family100'+m.chat].pesan = mesg }).catch(_ => _)
             if (isWin || isSurender) delete _family100['family100'+m.chat]
@@ -338,8 +361,8 @@ ${Array.from(room.jawaban, (jawaban, index) => {
 	    let winner = isSurrender ? room.game.currentTurn : room.game.winner
 	    let str = `Room ID: ${room.id}
 
-${arr.slice(0, 3).join('')}
-${arr.slice(3, 6).join('')}
+${arr.slice(0, 3).('')}
+${arr.slice(3, 6).('')}
 ${arr.slice(6).join('')}
 
 ${isWin ? `@${winner.split('@')[0]} Menang!` : isTie ? `Game berakhir` : `Giliran ${['❌', '⭕'][1 * room.game._currentTurn]} (@${room.game.currentTurn.split('@')[0]})`}
@@ -2084,7 +2107,7 @@ break
             caption: `Done!`,
             buttons: [{buttonId: `tiktokmp3 ${text}`, buttonText: { disText: "Audio" }, type: 1 }],
             footer: "To Change To Audio Use Manual #tiktokaudio [link]"
-            }, { quoted: m })
+            }, { quoted: ftoko })
 
             })
 
